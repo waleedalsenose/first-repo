2949767ec07877006b14e389cd5ad4cfaadffe60
waleed alsenose <willyalsenose@gmail.com>
2023-12-21 01:50:20 +0300

    adding models to the project

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    number_of_guests = models.PositiveIntegerField(default = 1)
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name}'s Booking"
+
+
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title  
